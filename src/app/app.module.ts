1f54e6b4f8644b60d1fb1ed1ba450ad37e7f6796
AKapran8 <kapranandriyu@gmail.com>
2021-02-10 18:01:31 +0200

    Create navigation menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
 import { NewUserComponent } from './components/new-user/new-user.component';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    NewUserComponent
+    NewUserComponent,
+    NavMenuComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,   
-    FormsModule, 
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
