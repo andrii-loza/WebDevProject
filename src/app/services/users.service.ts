1890d6e738b0f620556074164aa7e19d7fcea209
Andrii Loza <loza.andriy7@gmail.com>
2021-02-15 15:28:32 +0200

    after merge

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -44,25 +44,26 @@ export class UsersService {
       phone: '+4354534543'
     },
     {
-      firstName: "Shojol",
-      lastName: "Islam",
-      email: "me@shojol.com",
-      country: "Bangladesh",
-      timezone: "GMT +6",
-      city: "Dhaka",
+      firstName: 'Shojol',
+      lastName: 'Islam',
+      email: 'me@shojol.com',
+      country: 'Bangladesh',
+      timezone: 'GMT +6',
+      city: 'Dhaka',
       links: {
 
-        slack: "shojolislam",
-        email: "me@shojol.com"
+        slack: 'shojolislam',
+        email: 'me@shojol.com'
       },
       rate: 40,
       works_hours_limit: 22,
       jinn_score: 1243,
-      skills: ['AngularJs', 'React', '.NET', 'C++', "PHP", "React Native", "Angular"]
+      skills: ['AngularJs', 'React', '.NET', 'C++', 'PHP', 'React Native', 'Angular']
     }
   ];
 
-  emailForLog: string = 'me@shojol.com';//temporarily(need componet for login)
+  emailForLog = 'me@shojol.com';
+  // temporarily(need componet for login)
   constructor() { }
 
 
